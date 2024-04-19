6b8204dadfb779eb924f3f3413e36d7c0817c7b1
Bryan <bsamuel01@student.ciputra.ac.id>
2024-04-19 08:06:27 +0700

    BAB 1 part 2 step 7-8

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct LandmarkRow: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    var landmark: Landmark
+    
+    var body: some View{
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            
+            Spacer()
+        }
     }
 }
 
-#Preview {
-    LandmarkRow()
+#Preview("Turtle Rock") {
+    LandmarkRow(landmark: landmarks[0])
+}
+
+#Preview("Salmon") {
+    LandmarkRow(landmark: landmarks[1])
 }
